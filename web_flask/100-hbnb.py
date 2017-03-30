6e6659b8c3ce9b2e7bf4d94d8629631e6ce704f5
guptaNswati <sgupta1505@gmail.com>
2017-03-30 05:19:58 +0000

    adding 12

---
+++ web_flask/100-hbnb.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+This module contains a script that starts a Flask web application. And
+fetches data from database.
+"""
+from models import *
+from flask import Flask, render_template
+app = Flask(__name__)
+
+
+@app.route('/hbnb')
+def display_cities():
+    """ display a HTML page containing states, cities and amenities """
+    states = storage.all("State").values()
+    amenities = storage.all("Amenity").values()
+    places = storage.all("Place").values()
+    return render_template('100-hbnb.html', states=states,
+                           amenities=amenities, places=places)
+
+
+@app.teardown_appcontext
+def remove_session(exception=None):
+    """ remove the current SQLAlchemy Session """
+    storage.close()
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
